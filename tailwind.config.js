219cf87eac97a86728b538713f8ffbe5da58a2b0
namratapdr <namratapodder138@gmail.com>
2021-07-07 23:05:01 +0530

    Added Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+    prefix: '',
+    mode: 'jit',
+    purge: {
+      content: [
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+      container: {
+        center: true,
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+};
